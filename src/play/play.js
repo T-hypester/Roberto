4b9216b99eb8d018ee4bfed6dca6d7eebe30242c
Simone Cociancich <scociancich@cuebiq.com>
2023-04-19 19:26:59 +0200

    Pluggable sensor

--- src/play/play.js
+++ src/play/play.js
@@ -98,37 +98,7 @@ function play(level) {
         },
 
         look() {
-          const position = [...this.position];
-
-          const self = document.querySelector(
-            `.x${position[0]}.y${position[1]}`
-          );
-          self.classList.remove("fow");
-
-          const north = document.querySelector(
-            `.x${position[0]}.y${position[1] - 1}`
-          );
-          if (north && [0, 100, 300].includes(this.direction)) {
-            north.classList.remove("fow");
-          }
-          const east = document.querySelector(
-            `.x${position[0] + 1}.y${position[1]}`
-          );
-          if (east && [0, 100, 200].includes(this.direction)) {
-            east.classList.remove("fow");
-          }
-          const south = document.querySelector(
-            `.x${position[0]}.y${position[1] + 1}`
-          );
-          if (south && [100, 200, 300].includes(this.direction)) {
-            south.classList.remove("fow");
-          }
-          const west = document.querySelector(
-            `.x${position[0] - 1}.y${position[1]}`
-          );
-          if (west && [0, 200, 300].includes(this.direction)) {
-            west.classList.remove("fow");
-          }
+          this.sensor.look();
         },
 
         move(amount = Input.FORWARD) {
@@ -307,4 +277,7 @@ function play(level) {
 
 function setupRobot(tino) {
   tino.memory.size = parseInt(localStorage.getItem("ram"));
+  tino.sensor = new BasicSensor({
+    robot: tino,
+  });
 }
---
+++ src/play/sensors.ts
@@ -0,0 +1,43 @@
+class BasicSensor {
+    private robot: any
+
+    constructor(ctx: {
+        robot: any
+    }) {
+        Object.assign(this, ctx)
+    }
+
+    look() {
+        const position = [...this.robot.position];
+
+        const self = document.querySelector(
+          `.x${position[0]}.y${position[1]}`
+        );
+        self.classList.remove("fow");
+
+        const north = document.querySelector(
+          `.x${position[0]}.y${position[1] - 1}`
+        );
+        if (north && [0, 100, 300].includes(this.robot.direction)) {
+          north.classList.remove("fow");
+        }
+        const east = document.querySelector(
+          `.x${position[0] + 1}.y${position[1]}`
+        );
+        if (east && [0, 100, 200].includes(this.robot.direction)) {
+          east.classList.remove("fow");
+        }
+        const south = document.querySelector(
+          `.x${position[0]}.y${position[1] + 1}`
+        );
+        if (south && [100, 200, 300].includes(this.robot.direction)) {
+          south.classList.remove("fow");
+        }
+        const west = document.querySelector(
+          `.x${position[0] - 1}.y${position[1]}`
+        );
+        if (west && [0, 200, 300].includes(this.robot.direction)) {
+          west.classList.remove("fow");
+        }
+      }
+}
