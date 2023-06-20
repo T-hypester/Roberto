20a69d62ee93258a35b1e83f1e015ac487af1905
Simone Cociancich <scociancich@cuebiq.com>
2023-06-20 23:16:24 +0200

    Split splash and main screens

--- app/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", main)
-
-function main () {
-    const splash = document.getElementById("splash")
-    splash.addEventListener("click", () => {
-        splash.classList.add("clicked")
-    })
-}
